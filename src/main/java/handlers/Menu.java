20283260cf4ce8e791b838da11cdfab584402917
carrotchair <39000323+carrotchair@users.noreply.github.com>
2021-09-16 14:37:55 +0200

    Exercise finished

--- src/main/java/handlers/Menu.java
+++ src/main/java/handlers/Menu.java
@@ -7,6 +7,10 @@ import java.util.Scanner;
 public class Menu {
 
     public void menuShow() throws ParseException, IOException {
+        CreateCatalogStructure.createHomeFile();
+        CreateCatalogStructure.createDevFile();
+        CreateCatalogStructure.createTestFile();
+        CreateCatalogStructure.newCountFile();
         System.out.println("Welcome to file segregation. Let's get started.");
         System.out.println("You have multiple choice:");
 
@@ -15,31 +19,24 @@ public class Menu {
 
         while (!request.equals("5")) {
             CountFileNumber.countFileNumber();
-            System.out.println("1 Add catalogs");
-            System.out.println("2 New file");
-            System.out.println("3 Segregate all files from HOME catalog");
-            System.out.println("4 Number of files in specific directory");
-            System.out.println("5 End my work");
+            System.out.println("1 New file");
+            System.out.println("2 Segregate all files from HOME catalog");
+            System.out.println("3 Number of files in specific directory");
+            System.out.println("4 End my work");
             System.out.println("Choose a number:");
             request = scan.nextLine();
 
             switch (request) {
                 case "1":
-                    CreateCatalogStructure.createHomeFile();
-                    CreateCatalogStructure.createDevFile();
-                    CreateCatalogStructure.createTestFile();
-                    CreateCatalogStructure.newCountFile();
-                    break;
-                case "2":
                     NewFile.newFile();
                     break;
-                case "3":
+                case "2":
                     SegregateFilesFromHome.segregateFilesFromHomeDir();
                     break;
-                case "4":
+                case "3":
                     CountFileNumber.readFileNumber();
                     break;
-                case "5":
+                case "4":
                     System.out.println("See You next time!");
                     break;
                 default:
