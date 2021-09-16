fbb6b27db43bcf82df025f6745e18b93be6935d3
carrotchair <39000323+carrotchair@users.noreply.github.com>
2021-09-16 14:39:29 +0200

    Exercise finished

--- src/main/java/hello/SegregationApp.java
+++ src/main/java/hello/SegregationApp.java
@@ -5,7 +5,7 @@ import handlers.Menu;
 import java.io.IOException;
 import java.text.ParseException;
 
-public class HelloWorld {
+public class SegregationApp {
 
     public static void main(String[] args) throws ParseException, IOException {
         Menu menu = new Menu();
